7802c60ed47bb43d0011fa78c509d7436e8a5c53
Evidentsinger14 <cdelancey186423@gmail.com>
2025-10-13 19:04:11 -0400

    small additions to WarehouseManager

--- src/main/java/dev/ev1dent/perchWarehouse/managers/WarehouseManager.java
+++ src/main/java/dev/ev1dent/perchWarehouse/managers/WarehouseManager.java
@@ -28,7 +28,8 @@ public class WarehouseManager {
 
     public void openRegistration(int time){
         queueManager.isOpened = true;
-        TaskChain.newChain().delay(time).add(new TaskChain.GenericTask() {
+        LoggerUtil.debug("Queue Opened for " + time + " minutes");
+        TaskChain.newChain().delay(time * 1200).add(new TaskChain.GenericTask() {
 
             @Override
             protected void run() {
@@ -39,18 +40,28 @@ public class WarehouseManager {
     }
 
     public void start(){
-        LoggerUtil.debug("WarehouseManager.start()");
+        LoggerUtil.debug("Starting warehouse.");
         teleportPlayers();
-
+        LoggerUtil.debug("Closing Queue");
+        queueManager.isOpened = false;
     }
 
     public void stop(){
         LoggerUtil.debug("WarehouseManager.stop()");
+        //TODO teleport all players to their back location
+        queueManager.clearQueue();
+    }
+
+    private void updateBackLocation(Player player){
+        LoggerUtil.debug("Updating lastLocation for " + player.getName());
+        //TODO Hook into Essentials and set lastLocation. see PerchArtmap for how-to
     }
 
     private void teleportPlayers(){
         queueManager.getPlayersInQueue().forEach(playerUUID -> {
             Player player = Bukkit.getPlayer(playerUUID);
+            updateBackLocation(player);
+            //TODO get random spawn location from warehouses.yml random spawn locations
             Location location = new Location(player.getWorld(), player.getLocation().getX() + 100, player.getLocation().getY() + 100, player.getLocation().getZ() + 100);
             player.teleport(location);
 
